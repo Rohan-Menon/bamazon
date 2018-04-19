846e08f20e42309846af94cbf2f6b93722e2f994
Rohan Menon <rohan@Rohans-MacBook-Pro.local>
2018-04-19 16:56:39 -0500

    completed customer and manager functionality, also added product sales functionality to products table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,140 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table');
+
+var table;
 
 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bamazon'
+});
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+
+    displayProducts(true);
+
+});
+
+function displayProducts(firstRun) {
+
+    table = new Table({ head: ['ID', 'Product', 'Department', 'Price', 'Stock', 'Product Sales'] });
+
+
+    connection.query('SELECT * from products', function (error, results, fields) {
+        if (error) throw error;
+
+        results.forEach(result => {
+            table.push(
+                [result.item_id, result.product_name, result.department_name, "$" + result.price, result.stock_quantity, "$" + result.product_sales]
+            );
+        });
+
+
+        console.log(table.toString());
+
+        if (firstRun) {
+            purchasePrompt();
+        }
+        else {
+            programFlowPrompt();
+        }
+
+
+    });
+
+
+}
+
+
+function programFlowPrompt() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["Display Products", "Buy Something", "Quit"],
+            name: "userChoice"
+        }
+    ])
+        .then(function (answers) {
+            switch (answers.userChoice) {
+                case "Display Products":
+                    displayProducts();
+                    break;
+                case "Buy Something":
+                    purchasePrompt();
+                    break;
+                case "Quit":
+                    quit();
+                    break;
+                default:
+                    break;
+
+            }
+        })
+}
+
+function purchasePrompt() {
+
+
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Choose the id of the product you'd like the purchase",
+            name: "purchaseID"
+        },
+        {
+            type: "input",
+            message: "How many would you like to buy",
+            name: "purchaseQuantity"
+        }
+    ])
+        .then(function (answers) {
+            connection.query("SELECT * from products WHERE item_id = ?", [answers.purchaseID], function (err, results, fields) {
+
+                if (err) throw err;
+
+
+
+                if (results.length <= 0) {
+                    console.log(`
+                    Invalid ID
+                    `);
+                    programFlowPrompt();
+                }
+                else if (results[0].stock_quantity < answers.purchaseQuantity) {
+                    console.log(`
+                    Insufficient Stock
+                    `);
+                    programFlowPrompt();
+
+                }
+                else {
+                    var newStock = results[0].stock_quantity - answers.purchaseQuantity;
+                    var totalPrice = parseFloat(results[0].price) * answers.purchaseQuantity;
+
+                    connection.query("UPDATE products SET stock_quantity = ?, product_sales = product_sales + ? WHERE item_id = ?", [newStock, totalPrice, answers.purchaseID], function (err, results, fields) {
+                        if (err) throw err;
+
+
+
+                        console.log(`
+                        Total Price: $${totalPrice}
+                        `)
+                        programFlowPrompt();
+                    });
+                }
+            });
+
+        });
+
+
+}
+
+function quit() {
+    connection.end();
+}
---
+++ bamazonmanager.js
@@ -0,0 +1,176 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require('cli-table');
+
+var table;
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'bamazon'
+});
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+
+    managerPrompt();
+
+});
+
+function managerPrompt() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View Products For Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Quit"],
+            name: "managerChoice"
+        }
+    ])
+        .then(function (answers) {
+            switch (answers.managerChoice) {
+                case "View Products For Sale":
+                    displayProducts();
+                    break;
+                case "View Low Inventory":
+                    displayLowInventory();
+                    break;
+                case "Add to Inventory":
+                    addInventory();
+                    break;
+                case "Add New Product":
+                    addProduct();
+                    break;
+                case "Quit":
+                    quit();
+                    break;
+                default:
+                    break;
+
+            }
+        })
+}
+
+
+
+
+function displayProducts() {
+
+    table = new Table({ head: ['ID', 'Product', 'Department', 'Price', 'Stock', 'Product Sales'] });
+
+
+    connection.query('SELECT * from products', function (error, results, fields) {
+        if (error) throw error;
+
+        results.forEach(result => {
+            table.push(
+                [result.item_id, result.product_name, result.department_name, "$" + result.price, result.stock_quantity, "$" + result.product_sales]
+            );
+        });
+
+
+        console.log(table.toString());
+
+        managerPrompt();
+
+
+    });
+
+}
+
+function displayLowInventory(){
+
+    table = new Table({ head: ['ID', 'Product', 'Department', 'Price', 'Stock', 'Product Sales'] });
+
+    connection.query('SELECT * from products WHERE stock_quantity < ?',[5], function (error, results, fields) {
+        if (error) throw error;
+
+        results.forEach(result => {
+            table.push(
+                [result.item_id, result.product_name, result.department_name, "$" + result.price, result.stock_quantity, "$" + result.product_sales]
+            );
+        });
+
+
+        console.log(table.toString());
+
+        managerPrompt();
+
+
+    });
+
+}
+
+function addInventory(){
+
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Choose the id of the product",
+            name: "itemID"
+        },
+        {
+            type: "input",
+            message: "How much would you like to add?",
+            name: "addQuantity"
+        }
+    ])
+        .then(function (answers) {
+
+            connection.query("UPDATE products SET stock_quantity = stock_quantity + ? WHERE item_id = ?", [answers.addQuantity, answers.itemID], function (err, results, fields) {
+                if (err) throw err;
+
+                console.log(`
+                Inventory Added
+                `);
+
+                managerPrompt();
+            });
+        });
+}
+
+function addProduct(){
+
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Product Name: ",
+            name: "productName"
+        },
+        {
+            type: "input",
+            message: "Department: ",
+            name: "department"
+        },
+        {
+            type: "input",
+            message: "Price: ",
+            name: "price"
+        },
+        {
+            type: "input",
+            message: "Stock: ",
+            name: "stock"
+        }
+    ])
+    .then(function (answers) {
+
+        connection.query('INSERT INTO products(product_name, department_name, price, stock_quantity) VALUES (?,?,?,?)',[answers.productName, answers.department, answers.price, answers.stock], function (error, results, fields) {
+            if (error) throw error;
+
+            console.log(`
+            Added New Product!
+            `);
+    
+            managerPrompt();
+    
+    
+        });
+
+   });
+}
+
+function quit(){
+    connection.end();
+}
